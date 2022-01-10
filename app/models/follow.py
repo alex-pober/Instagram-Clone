670593115d79623cf2963dbaf74564373cf33db1
jaguitart <jaguitart@uc.cl>
2022-01-10 12:55:11 -0800

    db working

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,24 +1,9 @@
 from .db import db
-from datetime import datetime
+# from datetime import datetime
 
 
-class Follow(db.Model):
-    __tablename__ = 'follows'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    follower_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
-
-    follower = db.relationship("User", back_populates="followers")
-    followed = db.relationship("User", back_populates="followers")
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'follower_id': self.follower_id,
-            'followed_id': self.followed_id,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
-        }
+follows = db.Table(
+    "follows", 
+    db.Column("follower_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"))
+)
