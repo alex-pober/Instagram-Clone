d3979e34184bcf4a41fba5a0be1721c629183b3c
jaguitart <jaguitart@uc.cl>
2022-01-10 13:06:04 -0800

    profilE

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -22,7 +22,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('username', sa.String(length=50), nullable=False, unique=True),
-    sa.Column('profilURL', sa.String(length=2000)),
+    sa.Column('profileURL', sa.String(length=2000)),
     sa.Column('email', sa.String(length=255), nullable=False, unique=True),
     sa.Column('hashed_password', sa.String(length=255), nullable=False, unique=True),
     sa.Column('bio', sa.String(length=400)),
