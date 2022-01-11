0c91416178c43083cdce1a386eb5c749027a2281
Christopher Hauser <chauser6@gmail.com>
2022-01-11 16:38:03 -0500

    update for posts working

---
+++ react-app/src/components/posts/EditPostForm/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { updateOnePost } from "../../../store/posts";
+
+const EditPostForm = () => {
+    const history = useHistory();
+    const postId = useParams().id
+    const [errors, setErrors] = useState([]);
+    const [caption, setCaption] = useState('');
+    const oldCaption = useSelector(state => state?.posts[postId].caption)
+    const user = useSelector(state => state?.session.user);
+    const userId = useSelector(state => {
+        if (state.session.user) {
+            return state.session.user.id
+        }})
+    const image = useSelector(state => {
+        return state?.posts[postId].imgURL
+    })
+
+    useEffect(() => {
+        setCaption(oldCaption)
+    }, [])
+
+
+    const dispatch = useDispatch();
+
+    const updateCaption = e => {
+        setCaption(e.target.value)
+    }
+
+    const onEdit = async e => {
+        e.preventDefault()
+        const editedPost = {
+            id: +postId,
+            user_id: userId,
+            imgURL: image,
+            caption
+        }
+        let submitted = await dispatch(updateOnePost(editedPost))
+        if (submitted) {
+            history.push(`/posts/${postId}`)
+        }
+    }
+
+    return (
+        <form onSubmit={onEdit}>
+            <div>
+                <div>
+                    {errors.map((error, ind) => (
+                        <div key={ind}>{error}</div>
+                    ))}
+                </div>
+                <div>
+                    <img src={image}></img>
+                </div>
+                <div>
+                    <label htmlFor='caption'>Caption</label>
+                    <input
+                        name='caption'
+                        type='text'
+                        placeholder="Caption"
+                        value={caption}
+                        onChange={updateCaption}
+                    />
+                </div>
+                <button type='submit'>Post</button>
+            </div>
+        </form>
+    )
+}
+
+export default EditPostForm
