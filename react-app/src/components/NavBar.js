c2455ff5386cd5fbcd5bc5d7f63d0c607f471722
jaguitart <jaguitart@uc.cl>
2022-01-12 14:23:23 -0800

    demo user working remember to re-build the DB

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,13 +2,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import DemoButton from './auth/DemoUser';
 import { useSelector } from 'react-redux';
 
 const NavBar = () => {
   const userId = useSelector(state => {
     if (state.session.user) {
-        return state.session.user.id
-    }})
+      return state.session.user.id
+    }
+  })
 
   return (
     <nav>
@@ -19,28 +21,33 @@ const NavBar = () => {
           </NavLink>
         </li>
         {!userId && (
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-          )}
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+        )}
         {!userId && (
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-          )}
+          <li>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+        )}
         {/* <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </li> */}
         {userId && (
-        <li>
-          <LogoutButton />
-        </li>
+          <li>
+            <LogoutButton />
+          </li>
+        )}
+        {!userId && (
+          <li>
+            <DemoButton />
+          </li>
         )}
       </ul>
     </nav>
