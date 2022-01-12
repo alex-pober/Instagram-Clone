2fc2ff40ce64960d8df66d28670ce7df2ebe0470
droid97 <andresaguilarbazar@gmail.com>
2022-01-12 16:44:10 -0600

    footer

---
+++ react-app/src/components/footer/index.js
@@ -0,0 +1,29 @@
+
+
+const Footer = () => {
+
+    return(
+        <div className='footer-bar'>
+        <ul className="about-container">
+          <li className="about-container-info name">Alex Poverezhsiy{" "}</li>
+          <li className="about-container-info gitHub"><a href="https://github.com/alex-pober">GitHub</a></li>
+        </ul>
+        <ul className="about-container">
+          <li className="about-container-info name">Joaquin Guitart{" "}</li>
+          <li className="about-container-info gitHub"><a href="https://github.com/jaguitart">GitHub</a></li>
+        </ul>
+        <ul className="about-container">
+          <li className="about-container-info name">Christopher Hauser{" "}</li>
+          <li className="about-container-info gitHub"><a href="https://github.com/christopher-hauser">GitHub</a></li>
+        </ul>
+        <ul className="about-container">
+          <li className="about-container-info name">Andres Aguilar{" "}</li>
+          <li className="about-container-info gitHub"><a href="https://github.com/droid97">GitHub</a></li>
+        </ul>
+      </div>
+    );
+
+}
+
+
+export default Footer;
